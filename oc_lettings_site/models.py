b4cd5ec77c82054d54a480ba7a94be7b28f487dd
npaillasson <nathanpaillasson3@gmail.com>
2021-12-21 19:10:56 +0100

    change verbose_plural_name for Adress model. Now Adresss is not longer displayed in the django admin interface.

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -16,6 +16,9 @@ class Address(models.Model):
     def __str__(self):
         return f"{self.number} {self.street}"
 
+    class Meta:
+        verbose_name_plural = "Adresses"
+
 
 class Letting(models.Model):
     title = models.CharField(max_length=256)
