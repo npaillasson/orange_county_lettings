54ac12bf407467498e1178dd810ca06ee7533d0f
npaillasson <nathanpaillasson3@gmail.com>
2021-12-22 17:33:48 +0100

    the model profiles is totally out of place in the right application

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -26,11 +26,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="+")
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
