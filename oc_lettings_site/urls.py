d09c975e9c338bad523f5b24f7a09cfc6703082d
npaillasson <nathanpaillasson3@gmail.com>
2022-01-22 02:11:23 +0100

    code refactorization

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("sentry-debug", views.sentry_error, name="sentry_debug"),
+    path("sentry-debug/", views.sentry_error, name="sentry_debug"),
     path("lettings/", include(("lettings.urls", "lettings"), namespace="lettings")),
     path("profiles/", include(("profiles.urls", "profiles"), namespace="profiles")),
     path("admin/", admin.site.urls),
